1e9713c8f9b5f885ecdc84ed1fda49b79fe8ab0c
Sumit pawankar <Sumit pawankar@SUMIT>
2022-10-20 15:42:13 +0530

    Launching browser

--- src/main/java/Base_pom_Module/Amazon_Login_Page.java
+++ src/main/java/Base_pom_Module/Amazon_Login_Page.java
@@ -2,11 +2,16 @@ package Base_pom_Module;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-
+import org.testng.Reporter;
+import org.testng.Reporter;
 public class Amazon_Login_Page {
 
 	
 	public Amazon_Login_Page(WebDriver driver) {
 		PageFactory.initElements(driver, this);
+		
+		
+		Reporter.log("Launching amazon",true);
 	}
+	
 }
--- src/main/java/Base_pom_Module/Demo_class_student.java
+++ src/main/java/Base_pom_Module/Demo_class_student.java
@@ -11,14 +11,12 @@ public class Demo_class_student {
 		System.out.println("i am sumit");
 
 		System.out.println("i added in project");
-<<<<<<< HEAD
 		System.out.println("I am add in new project");
 		System.out.println(".............");
 
-=======
 		System.out.println("hey all sunny here iam add in new project ");
 		System.out.println("i m Bhagyashri");
->>>>>>> branch 'master' of https://github.com/SumitSP-95/Amazon_Framework.git
+
 	}
 	
 }
