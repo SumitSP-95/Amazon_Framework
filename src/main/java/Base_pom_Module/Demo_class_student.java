c1b5d72a27ccf7c8ad7d24238a4f9733bac73968
Sumit pawankar <Sumit pawankar@SUMIT>
2022-10-20 16:46:06 +0530

    i have added new code

--- src/main/java/Base_pom_Module/Demo_class_student.java
+++ src/main/java/Base_pom_Module/Demo_class_student.java
@@ -15,18 +15,15 @@ public class Demo_class_student {
 		System.out.println("i added in project");
 		System.out.println("I am add in new project");
 		System.out.println(".............");
-
 		System.out.println("hey all sunny here iam add in new project ");
-<<<<<<< HEAD
 		System.out.println("i m Bhagyashri");
-
-=======
 		System.out.println("checking status");
 		System.out.println("new status");
 		System.out.println("new one");
 		System.out.println("Hi gopal");
 		System.out.println("last one added");
->>>>>>> branch 'master' of https://github.com/SumitSP-95/Amazon_Framework.git
+		System.out.println("Sumit pawankar");
+
 	}
 	
 }
