39cf83be1ac22d3875982473f9b8200bb45dd3ff
hp <hp@DESKTOP-2R0H64G>
2022-10-20 14:47:22 +0530

    I Am Added

--- src/main/java/Base_pom_Module/Demo_class_student.java
+++ src/main/java/Base_pom_Module/Demo_class_student.java
@@ -6,6 +6,7 @@ public class Demo_class_student {
 	public static void main(String[] args) {
 		
 		System.out.println("This is new project according to Amazon");
+		System.out.println("i added in project");
 	}
 	
 }
