113a31e46d76fd6cdf728d90e80ef50e8894bd2b
HP <HP@HPP>
2022-10-20 15:32:27 +0530

    code add

--- src/main/java/Base_pom_Module/Demo_class_student.java
+++ src/main/java/Base_pom_Module/Demo_class_student.java
@@ -8,6 +8,6 @@ public class Demo_class_student {
 		System.out.println("This is new project according to Amazon");
 		System.out.println("i added in project");
 		System.out.println("hey all sunny here iam add in new project ");
+		System.out.println("code added by kunal");		
+
 	}
-	
-}
