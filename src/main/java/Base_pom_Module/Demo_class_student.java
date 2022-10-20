3d08c8518c999237cb5386339910cb93265c67e9
sunny <sunny@LAPTOP-V0UAGGMN>
2022-10-20 15:06:43 +0530

    replace name

--- src/main/java/Base_pom_Module/Demo_class_student.java
+++ src/main/java/Base_pom_Module/Demo_class_student.java
@@ -7,7 +7,7 @@ public class Demo_class_student {
 		
 		System.out.println("This is new project according to Amazon");
 		System.out.println("i added in project");
-		System.out.println("I am add in new project");
+		System.out.println("hey all sunny here iam add in new project ");
 	}
 	
 }
