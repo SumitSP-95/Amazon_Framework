ee9d2e0646e5833d996b6547ae5362572a47d968
Sumit pawankar <Sumit pawankar@SUMIT>
2022-10-20 15:05:17 +0530

    i am sumit

--- src/main/java/Base_pom_Module/Demo_class_student.java
+++ src/main/java/Base_pom_Module/Demo_class_student.java
@@ -6,6 +6,8 @@ public class Demo_class_student {
 	public static void main(String[] args) {
 		
 		System.out.println("This is new project according to Amazon");
+		System.out.println("Amazon project");
+		System.out.println("i am sumit");
 	}
 	
 }
