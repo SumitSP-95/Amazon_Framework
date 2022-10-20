ae9f635bbe67fe269cdd817e8098c8c9496461f9
user <user@LAPTOP-5CJSSFTQ>
2022-10-20 22:35:46 +0530

    properties method done by shubham

--- src/main/java/base_utility_Module/Base_class.java
+++ src/main/java/base_utility_Module/Base_class.java
@@ -1,5 +1,21 @@
 package base_utility_Module;
 
-public class Base_class {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
+public class Base_class {
+	
+	public static Properties properties;
+	
+	public static String getpropertyvalue(String key) throws IOException {
+		
+		properties = new Properties();
+		
+		FileInputStream file = new FileInputStream("C:\\Users\\user\\git\\Amazon_Framewor\\src\\test\\resources\\Property_File_Only\\Property_Amazon.properties");
+		
+		properties.load(file);
+		
+		return properties.getProperty(key);
+	}
 }
