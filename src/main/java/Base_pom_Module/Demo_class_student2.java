65df2809b8a709bb7dc8637a086aaa0c83ed06fe
Gopal <Gopal@LAPTOP-MROLQ548>
2022-10-20 22:10:07 +0530

    I have Complit my task Exclel sheet;

---
+++ src/main/java/Base_pom_Module/Demo_class_student2.java
@@ -0,0 +1,18 @@
+package Base_pom_Module;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Demo_class_student2 {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is new project according to Amazon");
+		System.out.println("i added in project");
+		System.out.println("hey all sunny here iam add in new project ");
+		System.out.println("checking status");
+		System.out.println("new status");
+		System.out.println("new one");
+	}
+	
+}
--- src/main/java/base_utility_Module/Utility_class.java
+++ src/main/java/base_utility_Module/Utility_class.java
@@ -1,5 +1,36 @@
 package base_utility_Module;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.WebDriver;
+
 public class Utility_class {
 
+	WebDriver driver;
+	public Utility_class(WebDriver _driver) {
+		driver = _driver;
+	}
+	
+	
+	public String Get_deta(int Row_Ind,int Column_Ind) throws EncryptedDocumentException, IOException {
+		
+		FileInputStream DCTC = new FileInputStream("C:\\Users\\Gopal\\Desktop\\Excle1\\Gopal.xlxe");
+	    
+		Sheet Amazon = WorkbookFactory.create(DCTC).getSheet("Amazon");
+		
+		Cell cell_Info = Amazon. getRow(Row_Ind).getCell(Column_Ind);
+		
+		try {
+			return cell_Info.getStringCellValue();
+		} catch (Exception e) {
+			return cell_Info.getNumericCellValue()+"";
+		}
+		
+	}
+	
 }
